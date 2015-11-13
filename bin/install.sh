18f7796aa6fa7437c27c2f410f67c3c61b748565
Heow Goodman <spamtrap@alphageeksinc.com>
2015-11-12 20:25:35 -0500

    make into shellscript

--- bin/install.sh
+++ bin/install.sh
@@ -1,10 +1,7 @@
-THE WAY WE WERE
-
+#!/bin/sh
 apt-get install dialog
 apt-get install fizmo-console
 apt-get install figlet
 apt-get install nethack-console
 
----
-
-(find-file "/pi@rpi:~/do.sh")
+# (find-file "/pi@rpi:~/do.sh")
